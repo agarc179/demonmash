20b7cceae3301743eb64c51b84c9477aa1c46f2e
CMURPH56 <CameronMurphy87@gmail.com>
2017-10-28 00:43:25 -0500

    rendering API call

--- src/components/Alerts.vue
+++ src/components/Alerts.vue
@@ -1,11 +1,42 @@
 <template>
   <div class="waiting">
     Not yet available
+    <head>
+    <title>Crime list</title>
+    </head>
+    <body>
+      <div id="app">
+        <li v-for="element in list">
+          <h1>{{list.description}}</h1>
+          <h2>{{list.block}}</h2>
+          <h3>{{list.date}}</h3>
+          <h4>{{list.arrest}}</h4>
+        </li>
+      </div>
+    </body>
   </div>
 </template>
 <script>
 export default {
 }
+</script>
+
+<script>
+ var link = 'https://data.cityofchicago.org/resource/crimes.json';
+ var data= {
+   list: null
+ };
+
+ Vue.http.get(link).then(function(response){
+   data.list =response.data;
+}, function(error){
+  console.log(error.statusText);
+});
+
+new Vue ({
+  el: '#app',
+  data: data
+});
 </script>
 <style scoped>
   .waiting {
---
+++ src/data.js
@@ -0,0 +1,9 @@
+
+ var link ='https://data.cityofchicago.org/resource/crimes.json';
+  
+export  default {
+
+    
+   
+
+}
