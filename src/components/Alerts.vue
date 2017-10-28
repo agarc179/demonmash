fee2d4e7f626379b1ab32adfc80f4fd003bb4b11
AdrianG <tej18359@gmail.com>
2017-10-28 09:37:38 -0500

    alerts page
    fixed alerts

--- src/components/Alerts.vue
+++ src/components/Alerts.vue
@@ -1,15 +1,50 @@
-<template>
-  <div class="waiting">
-    Not yet available
-  </div>
+<<template>
+  <table class="mdl-data-table mdl-js-data-table mdl-data-table--selectable mdl-shadow--2dp">
+  <thead>
+    <tr>
+      <th class="mdl-data-table__cell--non-numeric">Date</th>
+      <th>Discription</th>
+      <th>Block</th>
+      <th>District</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td class="mdl-data-table__cell--non-numeric">2017-10-19T23:50:00</td>
+      <td>FALSE/STOLEN/ALTERED TRP</td>
+      <td>047XX S WOLCOTT AVE</td>
+      <td>009</td>
+    </tr>
+    <tr>
+      <td class="mdl-data-table__cell--non-numeric">2017-10-19T23:50:00</td>
+      <td>AGGRAVATED: HANDGUN</td>
+      <td>022XX S AVERS AVE</td>
+      <td>010</td>
+    </tr>
+    <tr>
+      <td class="mdl-data-table__cell--non-numeric">2017-10-19T23:40:00</td>
+      <td>DOMESTIC BATTERY SIMPLE</td>
+      <td>049XX W SCHOOL ST</td>
+      <td>016</td>
+    </tr>
+    <tr>
+      <td class="mdl-data-table__cell--non-numeric">2017-10-19T23:39:00</td>
+      <td>SIMPLE</td>
+      <td>040XX W 79TH ST</td>
+      <td>008</td>
+    </tr>
+    <tr>
+      <td class="mdl-data-table__cell--non-numeric">2017-10-19T23:36:00</td>
+      <td>UNLAWFUL POSS OF HANDGUN</td>
+      <td>067XX S HALSTED ST</td>
+      <td>007</td>
+    </tr>
+    <tr>
+      <td class="mdl-data-table__cell--non-numeric">2017-10-19T23:30:00</td>
+      <td>OVER $500</td>
+      <td>018XX N MILWAUKEE AVE</td>
+      <td>014</td>
+    </tr>
+  </tbody>
+</table>
 </template>
-<script>
-export default {
-}
-</script>
-<style scoped>
-  .waiting {
-    padding: 10px;
-    color: #555;
-  }
-</style>
